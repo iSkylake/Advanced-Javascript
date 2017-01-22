5c45524a8b41c7c9355b96a7e142b661ba7a51fd
Llorch <SkylakeBeta@gmail.com>
2017-01-21 23:20:48 -0800

    Add settimeout function

--- bind.js
+++ bind.js
@@ -5,6 +5,11 @@ var llorch = {
 	},
 	addition: function(a,b,c,d){
 		console.log(this.firstName + " calculated 1+2+3+4=" + (a+b+c+d));
+	},
+	bindTimeOut: function(){
+		setTimeout(function(){
+			console.log("Hi " + this.firstName + " it binds");
+		}.bind(this), 1000);
 	}
 }
 
@@ -17,7 +22,10 @@ console.log("\n=============\nPASS ALL ARGS\n=============\n");
 var hanaAdd = llorch.addition.bind(hana, 1, 2, 3, 4);
 hanaAdd();
 
-console.log("\n==============\nPASS SOME ARGS\n==============\n")
+console.log("\n==============\nPASS SOME ARGS\n==============\n");
 
 var hanaAdd2 = llorch.addition.bind(hana, 1, 2);
 hanaAdd2(3, 4);
+
+console.log("\n=============\nSET TIME OUT\n=============\n");
+llorch.bindTimeOut();
